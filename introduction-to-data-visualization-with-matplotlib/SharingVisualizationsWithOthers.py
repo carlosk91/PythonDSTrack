aa47ab66eab90e55363761287d7e47307a028bcf
carlosk91 <carlosk0891@gmail.com>
2021-11-06 09:41:13 -0600

    20211028 - Finishing intro to matplotlib.

---
+++ introduction-to-data-visualization-with-matplotlib/SharingVisualizationsWithOthers.py
@@ -0,0 +1,50 @@
+# Exercise 1
+# Use the "ggplot" style and create new Figure/Axes
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-TAVG-NORMAL"])
+plt.show()
+
+# Exercise 2
+# Use the "Solarize_Light2" style and create new Figure/Axes
+plt.style.use('Solarize_Light2')
+fig, ax = plt.subplots()
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"])
+plt.show()
+
+# Exercise 3
+# Save as a PNG file
+fig.savefig('my_figure.png')
+
+# Save as a PNG file with 300 dpi
+fig.savefig('my_figure_300dpi.png', dpi=300)
+
+# Set figure dimensions and save as a PNG
+fig.set_size_inches([3,5])
+fig.savefig('figure_3_5.png')
+
+# Exercise 4
+# Extract the "Sport" column
+sports_column = summer_2016_medals['Sport']
+
+# Find the unique values of the "Sport" column
+sports = sports_column.unique()
+
+# Print out the unique sports values
+print(sports)
+
+# Exercise 5
+fig, ax = plt.subplots()
+
+# Loop over the different sports branches
+for sport in sports:
+  # Extract the rows only for this sport
+  sport_df = summer_2016_medals[summer_2016_medals['Sport'] == sport]
+  # Add a bar for the "Weight" mean with std y error bar
+  ax.bar(sport, sport_df['Weight'].mean(), yerr=sport_df['Weight'].std())
+
+ax.set_ylabel("Weight")
+ax.set_xticklabels(sports, rotation=90)
+
+# Save the figure to file
+fig.savefig('sports_weights.png')
